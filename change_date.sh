880bc7a06b7cfc44836bd6058a5e18d3ed250d35
Martin Roos <martin.roos@nordsoftware.com>
2018-08-10 15:05:55 +0300

    Added change_date.sh script

---
+++ change_date.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# A simple script for changing the created and modified dates of files based on the kMDItemContentCreationDate date 
+
+change_date () {
+	file=$1
+	if [[ $file == *.jpg ]] || [[ $file == *.png ]]
+        then
+		createdDate=$(mdls $file -name kMDItemFSCreationDate -raw)
+		newDate=$(mdls $file -name kMDItemContentCreationDate -raw)
+
+	        createdDateShow=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$createdDate" +"%d/%m/%Y %H:%M:%S %z")
+        	newDateShow=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$newDate" +"%d/%m/%Y %H:%M:%S %z")
+
+	        echo "$file: $createdDateShow -> $newDateShow"
+
+        	newDateFormated=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$newDate" +"%m/%d/%Y %H:%M:%S %z")
+	        SetFile -d "$newDateFormated" -m "$newDateFormated" $file
+	fi
+}
+
+loop_directory () {
+	for file in $1/*
+        do
+		if [ -d $file ]
+		then
+			loop_directory $file
+		else
+        		change_date $file
+		fi
+        done
+}
+
+if [ $# -eq 0 ]
+then
+	echo "A bash script for changing the created and modified dates of files based on the content created date"
+	echo "Usage: ./change_date.sh FILE|FOLDER"
+else
+	if [ -d $1 ]
+	then
+		loop_directory $1
+	else
+		change_date $1
+	fi
+fi
