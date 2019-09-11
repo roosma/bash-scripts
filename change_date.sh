51eec592779f2ccb751295504e9ca02c0a160d60
Martin Roos <martin.roos@nordsoftware.com>
2019-09-11 10:35:45 +0300

    Adding color, more info and taking into account spaces in folder- filenames

--- change_date.sh
+++ change_date.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 # A simple script for changing the created and modified dates of files based on the kMDItemContentCreationDate date 
 
+GREEN="\033[0;32m" # Green color
+YELLOW="\033[1;33m" # Yellow color
+BLUE="\033[0;35m" # Yellow color
+NC="\033[0m" # No Color
+OIFS="$IFS"
+IFS=$"\n"
+
 change_date () {
 	file=$1
 	if [[ $file == *.jpg ]] || [[ $file == *.png ]]
-        then
+	then
 		createdDate=$(mdls $file -name kMDItemFSCreationDate -raw)
 		newDate=$(mdls $file -name kMDItemContentCreationDate -raw)
 
-	        createdDateShow=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$createdDate" +"%d/%m/%Y %H:%M:%S %z")
-        	newDateShow=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$newDate" +"%d/%m/%Y %H:%M:%S %z")
+		if [[ $createdDate != $newDate ]]
+		then
+			createdDateShow=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$createdDate" +"%d/%m/%Y %H:%M:%S %z")
+			newDateShow=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$newDate" +"%d/%m/%Y %H:%M:%S %z")
 
-	        echo "$file: $createdDateShow -> $newDateShow"
+			printf "${file##*/}: ${YELLOW}${createdDateShow}${NC} -> ${GREEN}${newDateShow}${NC}\n"
 
-        	newDateFormated=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$newDate" +"%m/%d/%Y %H:%M:%S %z")
-	        SetFile -d "$newDateFormated" -m "$newDateFormated" $file
+			newDateFormated=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$newDate" +"%m/%d/%Y %H:%M:%S %z")
+			SetFile -d "$newDateFormated" -m "$newDateFormated" $file
+
+			changed=$((changed + 1))
+		fi
 	fi
 }
 
 loop_directory () {
 	for file in $1/*
-        do
+	do
 		if [ -d $file ]
 		then
+			printf "${BLUE}ENTERING DIRECTORY:${NC} ${file}\n"
 			loop_directory $file
 		else
-        		change_date $file
+			change_date $file
 		fi
-        done
+	done
 }
 
 if [ $# -eq 0 ]
@@ -38,8 +51,13 @@ then
 else
 	if [ -d $1 ]
 	then
+		changed=0
 		loop_directory $1
+
+		printf "\n${GREEN}${changed} FILES CHANGED${NC}\n\n"
 	else
 		change_date $1
 	fi
 fi
+
+IFS="$OIFS"
